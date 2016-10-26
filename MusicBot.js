29e8b1f32dcdc41b4688be9e4392bbf79cb5c097
tau6285 <alexanderf52598@gmail.com>
2016-10-26 18:13:12 -0500

    Add files via upload

--- MusicBot.js
+++ MusicBot.js
@@ -1,18 +1,14 @@
-
-
-
-var Discord = require("discord.js");
-
-var bot = new Discord.Client();
-
-bot.on("message", function(message))
-{
-	if (message.content === "/greeting")
-	{
-		bot.reply(message, "Welcome!")
-	}
-};
-
-bot.loginWithToken("MjQwOTY3OTEzNDUwODk3NDA5.CvLH0g.cQkWtEn5X_UJ9oN9IJKnbM3lVM8");
-
-bot.run
+var Bot = require('discord-bot');
+ 
+var bot = new Bot({
+    email: <email>,
+    password: <pass>
+});
+ 
+bot
+    .on(bot.triggers.command, 'hello')
+    .do(function(bot, conf, args) {
+        this.reply('world');
+    });
+ 
+bot.connect();
